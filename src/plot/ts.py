eee8713630d7614909d7b8f155dc7f8f325bfe5a
felipessalvatore <felipessalvatore@github.com>
2020-09-26 21:53:51 -0300

    add new basic exploration

--- src/plot/ts.py
+++ src/plot/ts.py
@@ -1,74 +1,12 @@
 import numpy as np
-import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import acovf
+from statsmodels.tsa.stattools import acf, ccf
 
 
-def get_sample_acvf(x, h):
-    """
-    x: series
-    h: shift param
-    return: autocovariance estimator
-    """
-    n = x.shape[0]
-    shift_x = x.shift(h)
-    mean = x.mean()
-    result = (x - mean) * (shift_x - mean)
-    return result.sum() / n
 
-
-def autocovariance_f(x, nlags):
-    """
-    x: series
-    nlags: range of lags param
-    return: array of autocovariance estimators
-    """
-    results = np.array([get_sample_acvf(x, h) for h in range(nlags)])
-    return results
-
-
-def autocorrelation_f(x, nlags):
-    """
-    x: series
-    nlags: range of lags param
-    return: array of autocorrelation estimators
-    """
-    gammas = autocovariance_f(x, nlags)
-    gamma_0 = get_sample_acvf(x, 0)
-    return gammas / gamma_0
-
-
-def get_sample_ccvf(x, y, h):
-    """
-    x: series
-    y: series
-    h: shift param
-    return: cross-covariance estimator
-    """
-    n = x.shape[0]
-    shift_x = x.shift(h)
-    mean_x = x.mean()
-    mean_y = y.mean()
-    result = (shift_x - mean_x) * (y - mean_y)
-    return result.sum() / n
-
-
-def crosscorrelation_f(x, y, nlags):
-    """
-    x: series
-    y: series
-    nlags: range of lags param
-    return: array of cross-correlation estimators
-    """
-    results = np.array([get_sample_ccvf(x, y, h) for h in range(nlags)])
-    gamma_x_0 = get_sample_acvf(x, 0)
-    gamma_y_0 = get_sample_acvf(y, 0)
-    denominator = np.sqrt(gamma_x_0 * gamma_y_0)
-    return results / denominator
-
-
-def plot_acf(x, lag_range, out_path,
-             acf_function=autocorrelation_f, reverse=True, figsize=(12, 5),
+def plot_acf(x, lag_range, reverse=True, figsize=(12, 5),
              title_fontsize=15, xlabel_fontsize=16, ylabel_fontsize=16):
     """
     plot autocorrelation of series x
@@ -94,7 +32,7 @@ def plot_acf(x, lag_range, out_path,
 
     title = "{}".format(x.name)
     lags = range(lag_range)
-    ac = acf_function(x, nlags=lag_range)
+    ac = acf(x,fft=False,nlags=lag_range)
     sigma = 1 / np.sqrt(x.shape[0])
     fig, ax = plt.subplots(figsize=figsize)
     ax.vlines(lags, [0], ac)
@@ -104,27 +42,20 @@ def plot_acf(x, lag_range, out_path,
     ax.set_xlabel('Lag', fontsize=xlabel_fontsize)
     ax.set_ylabel('autocorrelation', fontsize=ylabel_fontsize)
     fig.suptitle(title, fontsize=title_fontsize, fontweight='bold', y=0.93)
-    if out_path is not None:
-        plt.savefig(out_path)
+    
 
 
-def plot_ccf(x, y, lag_range, out_path,
-             ccf=crosscorrelation_f, reverse=True, figsize=(12, 5),
+def plot_ccf(x, y, lag_range,
+             figsize=(12, 5),
              title_fontsize=15, xlabel_fontsize=16, ylabel_fontsize=16):
     """
     plot cross-correlation between series x and y
-    :param x: series that we will perform the lag
+    :param x: series that we leads y on the left
     :type x: pd.Series
-    :param y: series
+    :param y: series that we leads x on the right
     :type y: pd.Series
     :param lag_range: range of lag
     :type lag_range: int
-    :param out_path: path to save figure
-    :type out_path: str
-    :param ccf: cross-correlation function
-    :type ccf: function
-    :param reverse: param to reverse lags
-    :type reverse: boolean
     :param figsize: figure size
     :type figsize: tuple
     :param title_fontsize: title font size
@@ -137,10 +68,11 @@ def plot_ccf(x, y, lag_range, out_path,
 
     title = "{} & {}".format(x.name, y.name)
     lags = range(-lag_range, lag_range + 1)
-    left = ccf(x, y, lag_range + 1)
-    rigt = ccf(y, x, lag_range)
+    left = ccf(y, x)[:lag_range + 1]
+    right = ccf(x, y)[:lag_range]
+
     left = left[1:][::-1]
-    cc = np.concatenate([left, rigt])
+    cc = np.concatenate([left, right])
 
     sigma = 1 / np.sqrt(x.shape[0])
     fig, ax = plt.subplots(figsize=figsize)
@@ -149,7 +81,43 @@ def plot_ccf(x, y, lag_range, out_path,
     plt.plot(lags, [2 * sigma] * len(lags), '-.', c="blue", linewidth=0.6)
     plt.plot(lags, [-2 * sigma] * len(lags), '-.', c="blue", linewidth=0.6)
     ax.set_xlabel('Lag', fontsize=xlabel_fontsize)
-    ax.set_ylabel('cross-correlation', fontsize=ylabel_fontsize)
+    ax.set_ylabel('cross-correlation', fontsize=ylabel_fontsize)    
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     fig.suptitle(title, fontsize=title_fontsize, fontweight='bold', y=0.93)
-    if out_path is not None:
-        plt.savefig(out_path)
+
+        
+def get_lead_matrix(lead_series, fixed_series, lag_range):
+    
+    """
+    get ccf vector of size 'max_lag' for each ts in 'lead_series' in
+    relation with 'fixed_series'. All the ccf results
+    are arranged in matrix format.
+
+    :param lead_series: list of series to be lagged. 
+                        All series are indexed by time.
+    :type lead_series: [pd.Series]
+    :param fixed_series: list of series indexed by time.
+    :type fixed_series: pd.Series
+    :param lag_range: range of lag
+    :type lag_range: int
+    :return: matrix of ccf information
+    :rtype: pd.DataFrame
+    """
+
+    ccf_rows = []
+    for ts in lead_series:
+        merged = pd.merge_asof(ts, fixed_series,
+                               left_index=True, right_index=True)
+
+        lagged_ts = merged[ts.name]
+        fixed_ts = merged[fixed_series.name]
+        row = ccf(fixed_ts, lagged_ts)[:lag_range +1]
+        ccf_rows.append(row)
+
+
+
+    ccf_matrix = np.array(ccf_rows)
+    ccf_matrix = pd.DataFrame(ccf_matrix,
+                              columns=["lag_{}".format(i) for i in range(lag_range +1)],
+                              index=[ts.name for ts in lead_series]) 
+    return ccf_matrix
