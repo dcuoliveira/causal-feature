1a5c6b4e9af5b6f67f352791640e8b159d07a13c
Daniel <dan.cunha@gmail.com>
2020-11-18 17:43:53 -0300

    add file format check and try

--- src/utils.py
+++ src/utils.py
@@ -20,22 +20,28 @@ def merge_data(df_list, freq='D'):
     return df_out
 
 
-def make_df_from_path(path, threshold_to_exclude=365):
+def make_df_from_path(path, threshold_to_exclude=365, file_format='.csv'):
     list_out = []
     excluded_list = []
     for file in os.listdir(path):
-        df_loop = pd.read_csv(os.path.join(path, file))
-        if len(df_loop) >= threshold_to_exclude:
-            df_loop = adj_find_target_str_bbg_data(df_loop)
-            names_in_col = [df_loop.columns[0]] + [file.replace('.csv', '')]
-            df_loop.columns = names_in_col
+        if file.endswith(file_format):
+            df_loop = pd.read_csv(os.path.join(path, file))
+            if len(df_loop) >= threshold_to_exclude:
+                df_loop = adj_find_target_str_bbg_data(df_loop)
+                names_in_col = [df_loop.columns[0]] + [file.replace('.csv', '')]
 
-            df_loop[names_in_col[0]] = pd.to_datetime(df_loop[names_in_col[0]])
-            df_loop[names_in_col[1]] = df_loop[names_in_col[1]].astype(float)
+                try:
+                    df_loop.columns = names_in_col
+                except:
+                    df_loop = df_loop[[names_in_col]]
 
-            df_loop = df_loop.set_index(names_in_col[0])
-            list_out.append(df_loop)
-        else:
-            excluded_list.append(file)
+                df_loop[names_in_col[0]] = pd.to_datetime(df_loop[names_in_col[0]])
+                df_loop[names_in_col[1]] = df_loop[names_in_col[1]].astype(float)
+
+                df_loop = df_loop.set_index(names_in_col[0])
+                list_out.append(df_loop)
+            else:
+                excluded_list.append(file)
     df_out = merge_data(list_out)
     return df_out, excluded_list
+
