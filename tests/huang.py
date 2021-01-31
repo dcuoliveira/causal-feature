d307495513ab3056aacaeb29bfef21040d11a937
Daniel Cunha Oliveira <dcuoliveira@Daniels-MacBook.local>
2021-01-30 23:46:17 -0300

    adjust tests

--- tests/huang.py
+++ tests/huang.py
@@ -246,14 +246,11 @@ class Test_huang(unittest.TestCase):
                                    words=["DOW JONES", "act", "arts", "bank", "business"], max_lag=20, verbose=False,
                                    sig_level=0.05, correl_threshold=0.5, constant_threshold=0.9)
 
+       
         self.assertAlmostEqual(result.loc[result['feature']=='DOW_JONES_11']['feature_score'].iloc[0],
-                               0.004244, places=3)
-        self.assertAlmostEqual(result.loc[result['feature']=='act_3']['feature_score'].iloc[0],
-                               0.004747, places=3)
-        self.assertAlmostEqual(result.loc[result['feature']=='business_5']['feature_score'].iloc[0],
-                               0.041698, places=3)
-        self.assertAlmostEqual(result.loc[result['feature']=='business_7']['feature_score'].iloc[0],
-                               0.010985, places=3)
+                                0.004244, places=3)
+        self.assertAlmostEqual(result.loc[result['feature']=='act_20']['feature_score'].iloc[0],
+                                0.023372, places=3)
 
     def test_huang_singular_matrix(self):
         path_m = os.path.join(parentdir,
@@ -269,13 +266,8 @@ class Test_huang(unittest.TestCase):
         result = run_huang_methods(merged_df=merged, target_name="target_return",
                                    words=full_words, max_lag=20, verbose=False,
                                    sig_level=0.05, correl_threshold=0.5, constant_threshold=0.9)
-        result = list(result[~result.feature_score.isna()].feature)
 
-        list_to_check = ['act_3', 'bonds_17', 'business_8', 'business_15', 'consume_1', 'debt_13',
-                         'firm_16', 'gain_6', 'greed_4', 'members_15', 'movement_1', 'nyse_3', 'nyse_5',
-                          'nyse_6', 'police_19', 'returns_4', 'success_5', 'world_9']
-        for w in list_to_check:
-            self.assertTrue(w in result)
+        self.assertTrue(result is None)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
