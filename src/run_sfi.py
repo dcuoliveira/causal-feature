1b8d9d0b7fef828b2f396688694579a3c5fde919
felipessalvatore <felipessalvatore@github.com>
2021-03-17 17:06:27 -0300

    add sfi results

--- src/run_sfi.py
+++ src/run_sfi.py
@@ -12,12 +12,12 @@ from data_mani.utils import get_ticker_name
 from data_mani.utils import path_filter
 
 # Variables
-N_SPLITS = 5 # number of CV splits
-N_CORES = 2 # number of cores to use
+N_SPLITS = 10 # number of CV splits
+N_CORES = 30 # number of cores to use
 MAX_LAG = 20 # maximum number of lags to create
              # google trends features
 OUT_FOLDER = "indices" # name of the marked data folder
-DEBUG = True # param to debug the script
+DEBUG = False # param to debug the script
 TEST_SIZE = 0.5 # pct of the train/test split
 THRESHOLD = 252 * 2 # treshold to filted merged datframes
                     # 252 = business days in a year
