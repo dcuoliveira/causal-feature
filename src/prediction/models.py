887363dde1b0231178db87dea21399501faceb54
dcuoliveira <dan.cunhaa@gmail.com>
2021-03-26 20:46:17 -0300

    add models

--- src/prediction/models.py
+++ src/prediction/models.py
@@ -1,7 +1,7 @@
 from scipy.stats import randint as sp_randint
 from scipy.stats import uniform as sp_uniform
 import numpy as np
-from sklearn.linear_model import Lasso, Ridge, ElasticNet
+from sklearn.linear_model import Lasso, Ridge, ElasticNet, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 
 
@@ -18,13 +18,43 @@ class RandomForestWrapper():
         else:
             self.ModelClass = RandomForestRegressor(**model_params)
             
-            
-class LassoWrapper():
-    def __init__(self, model_params=None):
-        self.model_name = "lasso"
-        self.search_type = 'grid'
-        self.param_grid = {'alpha': np.logspace(-4, 0, 50)}
+class LinearRegression():
+    def __init__(self, model_params={'fit_intercept': False}):
+        self.model_name = "linear_regression"
+        self.search_type = 'random'
+        self.param_grid = None
         if model_params is None:
             self.ModelClass = Lasso()
         else:
             self.ModelClass = Lasso(**model_params)
+                       
+class LassoWrapper():
+    def __init__(self, model_params={'fit_intercept': False}):
+        self.model_name = "lasso"
+        self.search_type = 'random'
+        self.param_grid = {'alpha': np.linspace(0, 1, 100)}
+        if model_params is None:
+            self.ModelClass = Lasso()
+        else:
+            self.ModelClass = Lasso(**model_params)
+            
+class RidgeWrapper():
+    def __init__(self, model_params={'fit_intercept': False}):
+        self.model_name = "ridge"
+        self.search_type = 'random'
+        self.param_grid = {'alpha': np.linspace(0, 1, 100)}
+        if model_params is None:
+            self.ModelClass = Ridge()
+        else:
+            self.ModelClass = Ridge(**model_params)
+            
+class ElasticNetWrapper():
+    def __init__(self, model_params={'fit_intercept': False}):
+        self.model_name = "elastic_net"
+        self.search_type = 'random'
+        self.param_grid = {'alpha': np.linspace(0, 1, 100),
+                           'l1_ratio': np.linspace(0, 1, 100)}
+        if model_params is None:
+            self.ModelClass = Ridge()
+        else:
+            self.ModelClass = Ridge(**model_params)
