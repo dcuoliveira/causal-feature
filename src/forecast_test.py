c39fc6d5d6f2b7260d0bb542e084bab75e9d7778
dcuoliveira <dan.cunhaa@gmail.com>
2021-03-26 16:04:42 -0300

    add lasso

--- src/forecast_test.py
+++ src/forecast_test.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time
 
 from data_mani.utils import merge_market_and_gtrends
-from prediction.models import RandomForestWrapper
+from prediction.models import RandomForestWrapper, LassoWrapper
 from prediction.functions import forecast
 
 if __name__ == '__main__':
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     init = time()
     pred_results = forecast(ticker_name="SPX Financial",
                             fs_method="sfi",
-                            Wrapper=RandomForestWrapper,
+                            Wrapper=LassoWrapper,
                             n_iter=10,
                             n_splits=5,
                             n_jobs=-1,
