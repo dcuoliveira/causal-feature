90c1260d30969e9a28b96b7e8406182c9c5d7334
dcuoliveira <dan.cunhaa@gmail.com>
2021-03-27 08:52:21 -0300

    change asset to SPX Index and export output

--- src/forecast_test.py
+++ src/forecast_test.py
@@ -10,12 +10,17 @@ from prediction.functions import forecast
 if __name__ == '__main__':
     
     init = time()
-    pred_results = forecast(ticker_name="SPX Financial",
+    pred_results = forecast(ticker_name="SPX Index",
                             fs_method="sfi",
                             Wrapper=LassoWrapper,
                             n_iter=10,
                             n_splits=5,
                             n_jobs=-1,
                             verbose=1)
+    pred_results.to_csv(os.path.join("results",
+                                     "forecast",
+                                     "sfi",
+                                     "indices",
+                                     "SPX Index.csv"))
     tempo = (time() - init) / 60
     print("total run time = ", np.round(tempo, 2), "min")    
