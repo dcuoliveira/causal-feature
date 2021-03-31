5e95442291dea8ac509924ca64dbfcf56753302a
felipessalvatore <felipessalvatore@github.com>
2021-03-31 10:23:18 -0300

    add new script maker

--- src/make_script.py
+++ src/make_script.py
@@ -16,14 +16,20 @@ model = args.model
 seeds = range(1, 50000)
 
 fs_methods = ["all", "sfi", "mdi", "mda", "huang", "granger", "IAMB", "MMMB"]
-
+indices = ["SPX Index", "CCMP Index", "RTY Index", "SPX Basic Materials",
+           "SPX Communications", "SPX Consumer Cyclical", "SPX Consumer Non cyclical",
+           "SPX Energy", "SPX Financial", "SPX Industrial", "SPX Technology",
+           "SPX Utilities"]
 
 
 with open("{}_forecast_script.sh".format(model), "w") as file:
-
-    for fs_method in fs_methods:
-        seed = np.random.choice(seeds)
-        command = "python3 forecast.py 'SPX Index' {} {} -S {}\n".format(fs_method,
-                                                                         model,
-                                                                         seed)
-        file.write(command)
+    for indice in indices:
+        comment = "# commands for {}\n".format(indice)
+        file.write(comment)
+        for fs_method in fs_methods:
+            seed = np.random.choice(seeds)
+            command = "python3 forecast.py '{}' {} {} -S {}\n".format(indice,
+                                                                      fs_method,
+                                                                      model,
+                                                                      seed)
+            file.write(command)
