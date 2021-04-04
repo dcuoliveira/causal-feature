98a33d15e5ca8ad53683a571aa5087483212100b
felipessalvatore <felipessalvatore@github.com>
2021-04-04 20:19:48 -0300

    new ridge script

--- src/ridge_forecast_script.sh
+++ src/ridge_forecast_script.sh
@@ -1,108 +1,108 @@
 # commands for SPX Index
-# python3 forecast.py 'SPX Index' all ridge -S 19555
-# python3 forecast.py 'SPX Index' sfi ridge -S 23040
-# python3 forecast.py 'SPX Index' mdi ridge -S 22971
-# python3 forecast.py 'SPX Index' mda ridge -S 18465
-# python3 forecast.py 'SPX Index' huang ridge -S 25280
-# python3 forecast.py 'SPX Index' granger ridge -S 44582
-# python3 forecast.py 'SPX Index' IAMB ridge -S 18309
-# python3 forecast.py 'SPX Index' MMMB ridge -S 10534
+python3 forecast.py 'SPX Index' all ridge -S 1549
+python3 forecast.py 'SPX Index' sfi ridge -S 44299
+python3 forecast.py 'SPX Index' mdi ridge -S 42582
+python3 forecast.py 'SPX Index' mda ridge -S 41141
+python3 forecast.py 'SPX Index' huang ridge -S 42168
+python3 forecast.py 'SPX Index' granger ridge -S 28228
+python3 forecast.py 'SPX Index' IAMB ridge -S 1560
+python3 forecast.py 'SPX Index' MMMB ridge -S 19525
 # commands for CCMP Index
-python3 forecast.py 'CCMP Index' all ridge -S 32223
-python3 forecast.py 'CCMP Index' sfi ridge -S 47870
-python3 forecast.py 'CCMP Index' mdi ridge -S 21716
-python3 forecast.py 'CCMP Index' mda ridge -S 5184
-python3 forecast.py 'CCMP Index' huang ridge -S 2149
-python3 forecast.py 'CCMP Index' granger ridge -S 2684
-python3 forecast.py 'CCMP Index' IAMB ridge -S 4273
-python3 forecast.py 'CCMP Index' MMMB ridge -S 34141
+python3 forecast.py 'CCMP Index' all ridge -S 22019
+python3 forecast.py 'CCMP Index' sfi ridge -S 14221
+python3 forecast.py 'CCMP Index' mdi ridge -S 25687
+python3 forecast.py 'CCMP Index' mda ridge -S 42993
+python3 forecast.py 'CCMP Index' huang ridge -S 27160
+python3 forecast.py 'CCMP Index' granger ridge -S 39637
+python3 forecast.py 'CCMP Index' IAMB ridge -S 6239
+python3 forecast.py 'CCMP Index' MMMB ridge -S 47822
 # commands for RTY Index
-python3 forecast.py 'RTY Index' all ridge -S 19049
-python3 forecast.py 'RTY Index' sfi ridge -S 42067
-python3 forecast.py 'RTY Index' mdi ridge -S 22464
-python3 forecast.py 'RTY Index' mda ridge -S 8259
-python3 forecast.py 'RTY Index' huang ridge -S 14768
-python3 forecast.py 'RTY Index' granger ridge -S 2837
-python3 forecast.py 'RTY Index' IAMB ridge -S 1897
-python3 forecast.py 'RTY Index' MMMB ridge -S 9344
+python3 forecast.py 'RTY Index' all ridge -S 17455
+python3 forecast.py 'RTY Index' sfi ridge -S 31474
+python3 forecast.py 'RTY Index' mdi ridge -S 27507
+python3 forecast.py 'RTY Index' mda ridge -S 22200
+python3 forecast.py 'RTY Index' huang ridge -S 46236
+python3 forecast.py 'RTY Index' granger ridge -S 41104
+python3 forecast.py 'RTY Index' IAMB ridge -S 32165
+python3 forecast.py 'RTY Index' MMMB ridge -S 20153
 # commands for SPX Basic Materials
-python3 forecast.py 'SPX Basic Materials' all ridge -S 15841
-python3 forecast.py 'SPX Basic Materials' sfi ridge -S 20950
-python3 forecast.py 'SPX Basic Materials' mdi ridge -S 40501
-python3 forecast.py 'SPX Basic Materials' mda ridge -S 46211
-python3 forecast.py 'SPX Basic Materials' huang ridge -S 41847
-python3 forecast.py 'SPX Basic Materials' granger ridge -S 24586
-python3 forecast.py 'SPX Basic Materials' IAMB ridge -S 6439
-python3 forecast.py 'SPX Basic Materials' MMMB ridge -S 22502
+python3 forecast.py 'SPX Basic Materials' all ridge -S 39941
+python3 forecast.py 'SPX Basic Materials' sfi ridge -S 425
+python3 forecast.py 'SPX Basic Materials' mdi ridge -S 10432
+python3 forecast.py 'SPX Basic Materials' mda ridge -S 26366
+python3 forecast.py 'SPX Basic Materials' huang ridge -S 35980
+python3 forecast.py 'SPX Basic Materials' granger ridge -S 30144
+python3 forecast.py 'SPX Basic Materials' IAMB ridge -S 16893
+python3 forecast.py 'SPX Basic Materials' MMMB ridge -S 28907
 # commands for SPX Communications
-python3 forecast.py 'SPX Communications' all ridge -S 680
-python3 forecast.py 'SPX Communications' sfi ridge -S 33135
-python3 forecast.py 'SPX Communications' mdi ridge -S 22334
-python3 forecast.py 'SPX Communications' mda ridge -S 43125
-python3 forecast.py 'SPX Communications' huang ridge -S 48765
-python3 forecast.py 'SPX Communications' granger ridge -S 9127
-python3 forecast.py 'SPX Communications' IAMB ridge -S 43278
-python3 forecast.py 'SPX Communications' MMMB ridge -S 46838
+python3 forecast.py 'SPX Communications' all ridge -S 43621
+python3 forecast.py 'SPX Communications' sfi ridge -S 8979
+python3 forecast.py 'SPX Communications' mdi ridge -S 47407
+python3 forecast.py 'SPX Communications' mda ridge -S 2666
+python3 forecast.py 'SPX Communications' huang ridge -S 35492
+python3 forecast.py 'SPX Communications' granger ridge -S 23736
+python3 forecast.py 'SPX Communications' IAMB ridge -S 4828
+python3 forecast.py 'SPX Communications' MMMB ridge -S 9871
 # commands for SPX Consumer Cyclical
-python3 forecast.py 'SPX Consumer Cyclical' all ridge -S 27384
-python3 forecast.py 'SPX Consumer Cyclical' sfi ridge -S 49842
-python3 forecast.py 'SPX Consumer Cyclical' mdi ridge -S 16935
-python3 forecast.py 'SPX Consumer Cyclical' mda ridge -S 20185
-python3 forecast.py 'SPX Consumer Cyclical' huang ridge -S 40887
-python3 forecast.py 'SPX Consumer Cyclical' granger ridge -S 28191
-python3 forecast.py 'SPX Consumer Cyclical' IAMB ridge -S 24169
-python3 forecast.py 'SPX Consumer Cyclical' MMMB ridge -S 21877
+python3 forecast.py 'SPX Consumer Cyclical' all ridge -S 30998
+python3 forecast.py 'SPX Consumer Cyclical' sfi ridge -S 48131
+python3 forecast.py 'SPX Consumer Cyclical' mdi ridge -S 49346
+python3 forecast.py 'SPX Consumer Cyclical' mda ridge -S 49445
+python3 forecast.py 'SPX Consumer Cyclical' huang ridge -S 86
+python3 forecast.py 'SPX Consumer Cyclical' granger ridge -S 38289
+python3 forecast.py 'SPX Consumer Cyclical' IAMB ridge -S 32189
+python3 forecast.py 'SPX Consumer Cyclical' MMMB ridge -S 26329
 # commands for SPX Consumer Non cyclical
-python3 forecast.py 'SPX Consumer Non cyclical' all ridge -S 34654
-python3 forecast.py 'SPX Consumer Non cyclical' sfi ridge -S 37093
-python3 forecast.py 'SPX Consumer Non cyclical' mdi ridge -S 7070
-python3 forecast.py 'SPX Consumer Non cyclical' mda ridge -S 15727
-python3 forecast.py 'SPX Consumer Non cyclical' huang ridge -S 18394
-python3 forecast.py 'SPX Consumer Non cyclical' granger ridge -S 45038
-python3 forecast.py 'SPX Consumer Non cyclical' IAMB ridge -S 16732
-python3 forecast.py 'SPX Consumer Non cyclical' MMMB ridge -S 11212
+python3 forecast.py 'SPX Consumer Non cyclical' all ridge -S 36000
+python3 forecast.py 'SPX Consumer Non cyclical' sfi ridge -S 4399
+python3 forecast.py 'SPX Consumer Non cyclical' mdi ridge -S 24057
+python3 forecast.py 'SPX Consumer Non cyclical' mda ridge -S 28277
+python3 forecast.py 'SPX Consumer Non cyclical' huang ridge -S 40952
+python3 forecast.py 'SPX Consumer Non cyclical' granger ridge -S 12851
+python3 forecast.py 'SPX Consumer Non cyclical' IAMB ridge -S 32589
+python3 forecast.py 'SPX Consumer Non cyclical' MMMB ridge -S 22632
 # commands for SPX Energy
-python3 forecast.py 'SPX Energy' all ridge -S 38742
-python3 forecast.py 'SPX Energy' sfi ridge -S 21722
-python3 forecast.py 'SPX Energy' mdi ridge -S 18069
-python3 forecast.py 'SPX Energy' mda ridge -S 44112
-python3 forecast.py 'SPX Energy' huang ridge -S 47436
-python3 forecast.py 'SPX Energy' granger ridge -S 38386
-python3 forecast.py 'SPX Energy' IAMB ridge -S 34492
-python3 forecast.py 'SPX Energy' MMMB ridge -S 7365
+python3 forecast.py 'SPX Energy' all ridge -S 18363
+python3 forecast.py 'SPX Energy' sfi ridge -S 43120
+python3 forecast.py 'SPX Energy' mdi ridge -S 33580
+python3 forecast.py 'SPX Energy' mda ridge -S 1646
+python3 forecast.py 'SPX Energy' huang ridge -S 18265
+python3 forecast.py 'SPX Energy' granger ridge -S 26404
+python3 forecast.py 'SPX Energy' IAMB ridge -S 35557
+python3 forecast.py 'SPX Energy' MMMB ridge -S 21849
 # commands for SPX Financial
-python3 forecast.py 'SPX Financial' all ridge -S 31873
-python3 forecast.py 'SPX Financial' sfi ridge -S 23661
-python3 forecast.py 'SPX Financial' mdi ridge -S 43726
-python3 forecast.py 'SPX Financial' mda ridge -S 12995
-python3 forecast.py 'SPX Financial' huang ridge -S 48292
-python3 forecast.py 'SPX Financial' granger ridge -S 3399
-python3 forecast.py 'SPX Financial' IAMB ridge -S 35022
-python3 forecast.py 'SPX Financial' MMMB ridge -S 38667
+python3 forecast.py 'SPX Financial' all ridge -S 17904
+python3 forecast.py 'SPX Financial' sfi ridge -S 18426
+python3 forecast.py 'SPX Financial' mdi ridge -S 37890
+python3 forecast.py 'SPX Financial' mda ridge -S 39441
+python3 forecast.py 'SPX Financial' huang ridge -S 48314
+python3 forecast.py 'SPX Financial' granger ridge -S 10309
+python3 forecast.py 'SPX Financial' IAMB ridge -S 20714
+python3 forecast.py 'SPX Financial' MMMB ridge -S 34770
 # commands for SPX Industrial
-python3 forecast.py 'SPX Industrial' all ridge -S 30595
-python3 forecast.py 'SPX Industrial' sfi ridge -S 32089
-python3 forecast.py 'SPX Industrial' mdi ridge -S 16145
-python3 forecast.py 'SPX Industrial' mda ridge -S 35070
-python3 forecast.py 'SPX Industrial' huang ridge -S 27201
-python3 forecast.py 'SPX Industrial' granger ridge -S 5805
-python3 forecast.py 'SPX Industrial' IAMB ridge -S 41619
-python3 forecast.py 'SPX Industrial' MMMB ridge -S 43222
+python3 forecast.py 'SPX Industrial' all ridge -S 11999
+python3 forecast.py 'SPX Industrial' sfi ridge -S 19215
+python3 forecast.py 'SPX Industrial' mdi ridge -S 20462
+python3 forecast.py 'SPX Industrial' mda ridge -S 43238
+python3 forecast.py 'SPX Industrial' huang ridge -S 22483
+python3 forecast.py 'SPX Industrial' granger ridge -S 557
+python3 forecast.py 'SPX Industrial' IAMB ridge -S 43239
+python3 forecast.py 'SPX Industrial' MMMB ridge -S 24250
 # commands for SPX Technology
-python3 forecast.py 'SPX Technology' all ridge -S 9209
-python3 forecast.py 'SPX Technology' sfi ridge -S 36392
-python3 forecast.py 'SPX Technology' mdi ridge -S 15161
-python3 forecast.py 'SPX Technology' mda ridge -S 42607
-python3 forecast.py 'SPX Technology' huang ridge -S 3268
-python3 forecast.py 'SPX Technology' granger ridge -S 14050
-python3 forecast.py 'SPX Technology' IAMB ridge -S 46464
-python3 forecast.py 'SPX Technology' MMMB ridge -S 6109
+python3 forecast.py 'SPX Technology' all ridge -S 14461
+python3 forecast.py 'SPX Technology' sfi ridge -S 27372
+python3 forecast.py 'SPX Technology' mdi ridge -S 31205
+python3 forecast.py 'SPX Technology' mda ridge -S 6936
+python3 forecast.py 'SPX Technology' huang ridge -S 46565
+python3 forecast.py 'SPX Technology' granger ridge -S 41057
+python3 forecast.py 'SPX Technology' IAMB ridge -S 19661
+python3 forecast.py 'SPX Technology' MMMB ridge -S 35330
 # commands for SPX Utilities
-python3 forecast.py 'SPX Utilities' all ridge -S 5798
-python3 forecast.py 'SPX Utilities' sfi ridge -S 17282
-python3 forecast.py 'SPX Utilities' mdi ridge -S 48764
-python3 forecast.py 'SPX Utilities' mda ridge -S 20926
-python3 forecast.py 'SPX Utilities' huang ridge -S 26782
-python3 forecast.py 'SPX Utilities' granger ridge -S 27662
-python3 forecast.py 'SPX Utilities' IAMB ridge -S 34411
-python3 forecast.py 'SPX Utilities' MMMB ridge -S 9069
+python3 forecast.py 'SPX Utilities' all ridge -S 12661
+python3 forecast.py 'SPX Utilities' sfi ridge -S 10509
+python3 forecast.py 'SPX Utilities' mdi ridge -S 20760
+python3 forecast.py 'SPX Utilities' mda ridge -S 2850
+python3 forecast.py 'SPX Utilities' huang ridge -S 4203
+python3 forecast.py 'SPX Utilities' granger ridge -S 49413
+python3 forecast.py 'SPX Utilities' IAMB ridge -S 15967
+python3 forecast.py 'SPX Utilities' MMMB ridge -S 44245
