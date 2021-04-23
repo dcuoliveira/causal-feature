f9919e3daf0a55798cb41bd0a2bdf7cb63c24ff3
dcuoliveira <dan.cunhaa@gmail.com>
2021-04-22 21:22:53 -0300

    add linear reg to debug

--- src/debug_forecast.py
+++ src/debug_forecast.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time
 
 from data_mani.utils import merge_market_and_gtrends
-from prediction.models import RandomForestWrapper, LassoWrapper
+from prediction.models import LinearGaussianRegressionWrapper, LassoWrapper
 from prediction.functions import forecast
 
 if __name__ == '__main__':
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     init = time()
     pred_results = forecast(ticker_name="SPX Index",
                             fs_method="sfi",
-                            Wrapper=LassoWrapper,
+                            Wrapper=LinearGaussianRegressionWrapper,
                             n_iter=10,
                             n_splits=5,
                             n_jobs=-1,
