56a71776bbd989f8ba8305d71c735ffc6cb3fc62
felipessalvatore <felipessalvatore@github.com>
2021-05-31 11:04:43 -0300

    add logit results

--- src/logit_forecast_script.sh
+++ src/logit_forecast_script.sh
@@ -1,108 +1,108 @@
-# commands for SPX Index -i 1
-python3 forecast.py 'SPX Index' all logit -S 331 -i 1
-python3 forecast.py 'SPX Index' sfi logit -S 44219 -i 1
-python3 forecast.py 'SPX Index' mdi logit -S 8098 -i 1
-python3 forecast.py 'SPX Index' mda logit -S 44432 -i 1
-python3 forecast.py 'SPX Index' huang logit -S 28051 -i 1
-python3 forecast.py 'SPX Index' granger logit -S 41149 -i 1
-python3 forecast.py 'SPX Index' IAMB logit -S 47411 -i 1
-python3 forecast.py 'SPX Index' MMMB logit -S 29109 -i 1
-# commands for CCMP Index -i 1
-python3 forecast.py 'CCMP Index' all logit -S 34066 -i 1
-python3 forecast.py 'CCMP Index' sfi logit -S 16975 -i 1
-python3 forecast.py 'CCMP Index' mdi logit -S 27519 -i 1
-python3 forecast.py 'CCMP Index' mda logit -S 5319 -i 1
-python3 forecast.py 'CCMP Index' huang logit -S 37578 -i 1
-python3 forecast.py 'CCMP Index' granger logit -S 20243 -i 1
-python3 forecast.py 'CCMP Index' IAMB logit -S 34963 -i 1
-python3 forecast.py 'CCMP Index' MMMB logit -S 16884 -i 1
-# commands for RTY Index -i 1
-python3 forecast.py 'RTY Index' all logit -S 34171 -i 1
-python3 forecast.py 'RTY Index' sfi logit -S 17954 -i 1
-python3 forecast.py 'RTY Index' mdi logit -S 39535 -i 1
-python3 forecast.py 'RTY Index' mda logit -S 31226 -i 1
-python3 forecast.py 'RTY Index' huang logit -S 15585 -i 1
-python3 forecast.py 'RTY Index' granger logit -S 5072 -i 1
-python3 forecast.py 'RTY Index' IAMB logit -S 42620 -i 1
-python3 forecast.py 'RTY Index' MMMB logit -S 14171 -i 1
-# commands for SPX Basic Materials -i 1
-python3 forecast.py 'SPX Basic Materials' all logit -S 42084 -i 1
-python3 forecast.py 'SPX Basic Materials' sfi logit -S 42743 -i 1
-python3 forecast.py 'SPX Basic Materials' mdi logit -S 44975 -i 1
-python3 forecast.py 'SPX Basic Materials' mda logit -S 33146 -i 1
-python3 forecast.py 'SPX Basic Materials' huang logit -S 49015 -i 1
-python3 forecast.py 'SPX Basic Materials' granger logit -S 36213 -i 1
-python3 forecast.py 'SPX Basic Materials' IAMB logit -S 28149 -i 1
-python3 forecast.py 'SPX Basic Materials' MMMB logit -S 31859 -i 1
-# commands for SPX Communications -i 1
-python3 forecast.py 'SPX Communications' all logit -S 20625 -i 1
-python3 forecast.py 'SPX Communications' sfi logit -S 22707 -i 1
-python3 forecast.py 'SPX Communications' mdi logit -S 34654 -i 1
-python3 forecast.py 'SPX Communications' mda logit -S 5716 -i 1
-python3 forecast.py 'SPX Communications' huang logit -S 1125 -i 1
-python3 forecast.py 'SPX Communications' granger logit -S 11575 -i 1
-python3 forecast.py 'SPX Communications' IAMB logit -S 566 -i 1
-python3 forecast.py 'SPX Communications' MMMB logit -S 27766 -i 1
-# commands for SPX Consumer Cyclical -i 1
-python3 forecast.py 'SPX Consumer Cyclical' all logit -S 34092 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' sfi logit -S 23271 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' mdi logit -S 1461 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' mda logit -S 34416 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' huang logit -S 27373 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' granger logit -S 5594 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' IAMB logit -S 30624 -i 1
-python3 forecast.py 'SPX Consumer Cyclical' MMMB logit -S 49822 -i 1
-# commands for SPX Consumer Non cyclical -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' all logit -S 22699 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' sfi logit -S 21068 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' mdi logit -S 11937 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' mda logit -S 30310 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' huang logit -S 3948 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' granger logit -S 44075 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' IAMB logit -S 21623 -i 1
-python3 forecast.py 'SPX Consumer Non cyclical' MMMB logit -S 43750 -i 1
-# commands for SPX Energy -i 1
-python3 forecast.py 'SPX Energy' all logit -S 4721 -i 1
-python3 forecast.py 'SPX Energy' sfi logit -S 9288 -i 1
-python3 forecast.py 'SPX Energy' mdi logit -S 32704 -i 1
-python3 forecast.py 'SPX Energy' mda logit -S 10344 -i 1
-python3 forecast.py 'SPX Energy' huang logit -S 11808 -i 1
-python3 forecast.py 'SPX Energy' granger logit -S 914 -i 1
-python3 forecast.py 'SPX Energy' IAMB logit -S 47119 -i 1
-python3 forecast.py 'SPX Energy' MMMB logit -S 6544 -i 1
-# commands for SPX Financial -i 1
-python3 forecast.py 'SPX Financial' all logit -S 25920 -i 1
-python3 forecast.py 'SPX Financial' sfi logit -S 1198 -i 1
-python3 forecast.py 'SPX Financial' mdi logit -S 9687 -i 1
-python3 forecast.py 'SPX Financial' mda logit -S 3299 -i 1
-python3 forecast.py 'SPX Financial' huang logit -S 29121 -i 1
-python3 forecast.py 'SPX Financial' granger logit -S 42402 -i 1
-python3 forecast.py 'SPX Financial' IAMB logit -S 28589 -i 1
-python3 forecast.py 'SPX Financial' MMMB logit -S 23042 -i 1
-# commands for SPX Industrial -i 1
-python3 forecast.py 'SPX Industrial' all logit -S 3139 -i 1
-python3 forecast.py 'SPX Industrial' sfi logit -S 15591 -i 1
-python3 forecast.py 'SPX Industrial' mdi logit -S 8733 -i 1
-python3 forecast.py 'SPX Industrial' mda logit -S 43185 -i 1
-python3 forecast.py 'SPX Industrial' huang logit -S 27830 -i 1
-python3 forecast.py 'SPX Industrial' granger logit -S 14212 -i 1
-python3 forecast.py 'SPX Industrial' IAMB logit -S 7876 -i 1
-python3 forecast.py 'SPX Industrial' MMMB logit -S 40937 -i 1
-# commands for SPX Technology -i 1
-python3 forecast.py 'SPX Technology' all logit -S 15012 -i 1
-python3 forecast.py 'SPX Technology' sfi logit -S 7439 -i 1
-python3 forecast.py 'SPX Technology' mdi logit -S 41077 -i 1
-python3 forecast.py 'SPX Technology' mda logit -S 48262 -i 1
-python3 forecast.py 'SPX Technology' huang logit -S 10194 -i 1
-python3 forecast.py 'SPX Technology' granger logit -S 334 -i 1
-python3 forecast.py 'SPX Technology' IAMB logit -S 44767 -i 1
-python3 forecast.py 'SPX Technology' MMMB logit -S 38107 -i 1
-# commands for SPX Utilities -i 1
-python3 forecast.py 'SPX Utilities' all logit -S 6041 -i 1
-python3 forecast.py 'SPX Utilities' sfi logit -S 21931 -i 1
-python3 forecast.py 'SPX Utilities' mdi logit -S 25944 -i 1
-python3 forecast.py 'SPX Utilities' mda logit -S 5110 -i 1
-python3 forecast.py 'SPX Utilities' huang logit -S 21393 -i 1
-python3 forecast.py 'SPX Utilities' granger logit -S 20629 -i 1
-python3 forecast.py 'SPX Utilities' IAMB logit -S 9413 -i 1
-python3 forecast.py 'SPX Utilities' MMMB logit -S 823 -i 1
+# commands for SPX Index
+python3 forecast.py 'SPX Index' all logit -S 3452 -i 1
+python3 forecast.py 'SPX Index' sfi logit -S 20467 -i 1
+python3 forecast.py 'SPX Index' mdi logit -S 39106 -i 1
+python3 forecast.py 'SPX Index' mda logit -S 41715 -i 1
+python3 forecast.py 'SPX Index' huang logit -S 21230 -i 1
+python3 forecast.py 'SPX Index' granger logit -S 18212 -i 1
+python3 forecast.py 'SPX Index' IAMB logit -S 11690 -i 1
+python3 forecast.py 'SPX Index' MMMB logit -S 24526 -i 1
+# commands for CCMP Index
+python3 forecast.py 'CCMP Index' all logit -S 41277 -i 1
+python3 forecast.py 'CCMP Index' sfi logit -S 21252 -i 1
+python3 forecast.py 'CCMP Index' mdi logit -S 42703 -i 1
+python3 forecast.py 'CCMP Index' mda logit -S 14041 -i 1
+python3 forecast.py 'CCMP Index' huang logit -S 12157 -i 1
+python3 forecast.py 'CCMP Index' granger logit -S 24202 -i 1
+python3 forecast.py 'CCMP Index' IAMB logit -S 16133 -i 1
+python3 forecast.py 'CCMP Index' MMMB logit -S 29840 -i 1
+# commands for RTY Index
+python3 forecast.py 'RTY Index' all logit -S 30718 -i 1
+python3 forecast.py 'RTY Index' sfi logit -S 15403 -i 1
+python3 forecast.py 'RTY Index' mdi logit -S 1120 -i 1
+python3 forecast.py 'RTY Index' mda logit -S 10871 -i 1
+python3 forecast.py 'RTY Index' huang logit -S 3806 -i 1
+python3 forecast.py 'RTY Index' granger logit -S 5364 -i 1
+python3 forecast.py 'RTY Index' IAMB logit -S 47094 -i 1
+python3 forecast.py 'RTY Index' MMMB logit -S 11044 -i 1
+# commands for SPX Basic Materials
+python3 forecast.py 'SPX Basic Materials' all logit -S 12451 -i 1
+python3 forecast.py 'SPX Basic Materials' sfi logit -S 26557 -i 1
+python3 forecast.py 'SPX Basic Materials' mdi logit -S 48924 -i 1
+python3 forecast.py 'SPX Basic Materials' mda logit -S 2160 -i 1
+python3 forecast.py 'SPX Basic Materials' huang logit -S 27695 -i 1
+python3 forecast.py 'SPX Basic Materials' granger logit -S 45306 -i 1
+python3 forecast.py 'SPX Basic Materials' IAMB logit -S 3900 -i 1
+python3 forecast.py 'SPX Basic Materials' MMMB logit -S 3807 -i 1
+# commands for SPX Communications
+python3 forecast.py 'SPX Communications' all logit -S 40852 -i 1
+python3 forecast.py 'SPX Communications' sfi logit -S 773 -i 1
+python3 forecast.py 'SPX Communications' mdi logit -S 12029 -i 1
+python3 forecast.py 'SPX Communications' mda logit -S 38071 -i 1
+python3 forecast.py 'SPX Communications' huang logit -S 37224 -i 1
+python3 forecast.py 'SPX Communications' granger logit -S 7079 -i 1
+python3 forecast.py 'SPX Communications' IAMB logit -S 24722 -i 1
+python3 forecast.py 'SPX Communications' MMMB logit -S 38111 -i 1
+# commands for SPX Consumer Cyclical
+python3 forecast.py 'SPX Consumer Cyclical' all logit -S 45001 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' sfi logit -S 15670 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' mdi logit -S 46296 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' mda logit -S 18429 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' huang logit -S 2893 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' granger logit -S 15957 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' IAMB logit -S 22124 -i 1
+python3 forecast.py 'SPX Consumer Cyclical' MMMB logit -S 37461 -i 1
+# commands for SPX Consumer Non cyclical
+python3 forecast.py 'SPX Consumer Non cyclical' all logit -S 41924 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' sfi logit -S 30470 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' mdi logit -S 12640 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' mda logit -S 46892 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' huang logit -S 15999 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' granger logit -S 33011 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' IAMB logit -S 28558 -i 1
+python3 forecast.py 'SPX Consumer Non cyclical' MMMB logit -S 45191 -i 1
+# commands for SPX Energy
+python3 forecast.py 'SPX Energy' all logit -S 26553 -i 1
+python3 forecast.py 'SPX Energy' sfi logit -S 16407 -i 1
+python3 forecast.py 'SPX Energy' mdi logit -S 47940 -i 1
+python3 forecast.py 'SPX Energy' mda logit -S 3632 -i 1
+python3 forecast.py 'SPX Energy' huang logit -S 1193 -i 1
+python3 forecast.py 'SPX Energy' granger logit -S 45836 -i 1
+python3 forecast.py 'SPX Energy' IAMB logit -S 14510 -i 1
+python3 forecast.py 'SPX Energy' MMMB logit -S 7128 -i 1
+# commands for SPX Financial
+python3 forecast.py 'SPX Financial' all logit -S 1209 -i 1
+python3 forecast.py 'SPX Financial' sfi logit -S 24947 -i 1
+python3 forecast.py 'SPX Financial' mdi logit -S 21761 -i 1
+python3 forecast.py 'SPX Financial' mda logit -S 2295 -i 1
+python3 forecast.py 'SPX Financial' huang logit -S 14018 -i 1
+python3 forecast.py 'SPX Financial' granger logit -S 44760 -i 1
+python3 forecast.py 'SPX Financial' IAMB logit -S 34957 -i 1
+python3 forecast.py 'SPX Financial' MMMB logit -S 25930 -i 1
+# commands for SPX Industrial
+python3 forecast.py 'SPX Industrial' all logit -S 24242 -i 1
+python3 forecast.py 'SPX Industrial' sfi logit -S 26535 -i 1
+python3 forecast.py 'SPX Industrial' mdi logit -S 40258 -i 1
+python3 forecast.py 'SPX Industrial' mda logit -S 28022 -i 1
+python3 forecast.py 'SPX Industrial' huang logit -S 453 -i 1
+python3 forecast.py 'SPX Industrial' granger logit -S 21675 -i 1
+python3 forecast.py 'SPX Industrial' IAMB logit -S 21760 -i 1
+python3 forecast.py 'SPX Industrial' MMMB logit -S 4688 -i 1
+# commands for SPX Technology
+python3 forecast.py 'SPX Technology' all logit -S 13917 -i 1
+python3 forecast.py 'SPX Technology' sfi logit -S 48129 -i 1
+python3 forecast.py 'SPX Technology' mdi logit -S 8452 -i 1
+python3 forecast.py 'SPX Technology' mda logit -S 32440 -i 1
+python3 forecast.py 'SPX Technology' huang logit -S 18144 -i 1
+python3 forecast.py 'SPX Technology' granger logit -S 17925 -i 1
+python3 forecast.py 'SPX Technology' IAMB logit -S 1593 -i 1
+python3 forecast.py 'SPX Technology' MMMB logit -S 40127 -i 1
+# commands for SPX Utilities
+python3 forecast.py 'SPX Utilities' all logit -S 29507 -i 1
+python3 forecast.py 'SPX Utilities' sfi logit -S 3865 -i 1
+python3 forecast.py 'SPX Utilities' mdi logit -S 39760 -i 1
+python3 forecast.py 'SPX Utilities' mda logit -S 33247 -i 1
+python3 forecast.py 'SPX Utilities' huang logit -S 15867 -i 1
+python3 forecast.py 'SPX Utilities' granger logit -S 38581 -i 1
+python3 forecast.py 'SPX Utilities' IAMB logit -S 5019 -i 1
+python3 forecast.py 'SPX Utilities' MMMB logit -S 35958 -i 1
