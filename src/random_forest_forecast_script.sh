a6e8b7dd41de5a549b7eb6ce94c4953d5efc38cf
felipessalvatore <felipessalvatore@github.com>
2021-06-03 18:26:47 -0300

    add rf results

--- src/random_forest_forecast_script.sh
+++ src/random_forest_forecast_script.sh
@@ -1,108 +1,108 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all random_forest -S 36602
-python3 forecast.py 'SPX Index' sfi random_forest -S 755
-python3 forecast.py 'SPX Index' mdi random_forest -S 42791
-python3 forecast.py 'SPX Index' mda random_forest -S 30932
-python3 forecast.py 'SPX Index' huang random_forest -S 28227
-python3 forecast.py 'SPX Index' granger random_forest -S 7750
-python3 forecast.py 'SPX Index' IAMB random_forest -S 2539
-python3 forecast.py 'SPX Index' MMMB random_forest -S 21158
+python3 forecast.py 'SPX Index' all random_forest -S 44467
+python3 forecast.py 'SPX Index' sfi random_forest -S 26868
+python3 forecast.py 'SPX Index' mdi random_forest -S 14552
+python3 forecast.py 'SPX Index' mda random_forest -S 11566
+python3 forecast.py 'SPX Index' huang random_forest -S 45205
+python3 forecast.py 'SPX Index' granger random_forest -S 34783
+python3 forecast.py 'SPX Index' IAMB random_forest -S 35696
+python3 forecast.py 'SPX Index' MMMB random_forest -S 4870
 # commands for CCMP Index
-python3 forecast.py 'CCMP Index' all random_forest -S 28783
-python3 forecast.py 'CCMP Index' sfi random_forest -S 41206
-python3 forecast.py 'CCMP Index' mdi random_forest -S 32374
-python3 forecast.py 'CCMP Index' mda random_forest -S 37265
-python3 forecast.py 'CCMP Index' huang random_forest -S 29592
-python3 forecast.py 'CCMP Index' granger random_forest -S 9633
-python3 forecast.py 'CCMP Index' IAMB random_forest -S 25579
-python3 forecast.py 'CCMP Index' MMMB random_forest -S 49956
+python3 forecast.py 'CCMP Index' all random_forest -S 10665
+python3 forecast.py 'CCMP Index' sfi random_forest -S 16113
+python3 forecast.py 'CCMP Index' mdi random_forest -S 13765
+python3 forecast.py 'CCMP Index' mda random_forest -S 33582
+python3 forecast.py 'CCMP Index' huang random_forest -S 48724
+python3 forecast.py 'CCMP Index' granger random_forest -S 5122
+python3 forecast.py 'CCMP Index' IAMB random_forest -S 34560
+python3 forecast.py 'CCMP Index' MMMB random_forest -S 23511
 # commands for RTY Index
-python3 forecast.py 'RTY Index' all random_forest -S 2819
-python3 forecast.py 'RTY Index' sfi random_forest -S 33623
-python3 forecast.py 'RTY Index' mdi random_forest -S 43096
-python3 forecast.py 'RTY Index' mda random_forest -S 18590
-python3 forecast.py 'RTY Index' huang random_forest -S 44698
-python3 forecast.py 'RTY Index' granger random_forest -S 48025
-python3 forecast.py 'RTY Index' IAMB random_forest -S 7506
-python3 forecast.py 'RTY Index' MMMB random_forest -S 5576
+python3 forecast.py 'RTY Index' all random_forest -S 12386
+python3 forecast.py 'RTY Index' sfi random_forest -S 18118
+python3 forecast.py 'RTY Index' mdi random_forest -S 13549
+python3 forecast.py 'RTY Index' mda random_forest -S 10347
+python3 forecast.py 'RTY Index' huang random_forest -S 8464
+python3 forecast.py 'RTY Index' granger random_forest -S 23610
+python3 forecast.py 'RTY Index' IAMB random_forest -S 36764
+python3 forecast.py 'RTY Index' MMMB random_forest -S 25205
 # commands for SPX Basic Materials
-python3 forecast.py 'SPX Basic Materials' all random_forest -S 41043
-python3 forecast.py 'SPX Basic Materials' sfi random_forest -S 36197
-python3 forecast.py 'SPX Basic Materials' mdi random_forest -S 33354
-python3 forecast.py 'SPX Basic Materials' mda random_forest -S 46315
-python3 forecast.py 'SPX Basic Materials' huang random_forest -S 38051
-python3 forecast.py 'SPX Basic Materials' granger random_forest -S 18469
-python3 forecast.py 'SPX Basic Materials' IAMB random_forest -S 49331
-python3 forecast.py 'SPX Basic Materials' MMMB random_forest -S 48118
+python3 forecast.py 'SPX Basic Materials' all random_forest -S 29126
+python3 forecast.py 'SPX Basic Materials' sfi random_forest -S 28364
+python3 forecast.py 'SPX Basic Materials' mdi random_forest -S 37956
+python3 forecast.py 'SPX Basic Materials' mda random_forest -S 15658
+python3 forecast.py 'SPX Basic Materials' huang random_forest -S 49790
+python3 forecast.py 'SPX Basic Materials' granger random_forest -S 32382
+python3 forecast.py 'SPX Basic Materials' IAMB random_forest -S 25184
+python3 forecast.py 'SPX Basic Materials' MMMB random_forest -S 34120
 # commands for SPX Communications
-python3 forecast.py 'SPX Communications' all random_forest -S 22044
-python3 forecast.py 'SPX Communications' sfi random_forest -S 15454
-python3 forecast.py 'SPX Communications' mdi random_forest -S 24907
-python3 forecast.py 'SPX Communications' mda random_forest -S 24224
-python3 forecast.py 'SPX Communications' huang random_forest -S 27694
-python3 forecast.py 'SPX Communications' granger random_forest -S 5581
-python3 forecast.py 'SPX Communications' IAMB random_forest -S 24067
-python3 forecast.py 'SPX Communications' MMMB random_forest -S 5518
+python3 forecast.py 'SPX Communications' all random_forest -S 17523
+python3 forecast.py 'SPX Communications' sfi random_forest -S 17018
+python3 forecast.py 'SPX Communications' mdi random_forest -S 43272
+python3 forecast.py 'SPX Communications' mda random_forest -S 5082
+python3 forecast.py 'SPX Communications' huang random_forest -S 20091
+python3 forecast.py 'SPX Communications' granger random_forest -S 45885
+python3 forecast.py 'SPX Communications' IAMB random_forest -S 14827
+python3 forecast.py 'SPX Communications' MMMB random_forest -S 44247
 # commands for SPX Consumer Cyclical
-python3 forecast.py 'SPX Consumer Cyclical' all random_forest -S 30274
-python3 forecast.py 'SPX Consumer Cyclical' sfi random_forest -S 44969
-python3 forecast.py 'SPX Consumer Cyclical' mdi random_forest -S 48284
-python3 forecast.py 'SPX Consumer Cyclical' mda random_forest -S 20289
-python3 forecast.py 'SPX Consumer Cyclical' huang random_forest -S 37310
-python3 forecast.py 'SPX Consumer Cyclical' granger random_forest -S 30250
-python3 forecast.py 'SPX Consumer Cyclical' IAMB random_forest -S 7045
-python3 forecast.py 'SPX Consumer Cyclical' MMMB random_forest -S 40201
+python3 forecast.py 'SPX Consumer Cyclical' all random_forest -S 13152
+python3 forecast.py 'SPX Consumer Cyclical' sfi random_forest -S 22805
+python3 forecast.py 'SPX Consumer Cyclical' mdi random_forest -S 32456
+python3 forecast.py 'SPX Consumer Cyclical' mda random_forest -S 29775
+python3 forecast.py 'SPX Consumer Cyclical' huang random_forest -S 45664
+python3 forecast.py 'SPX Consumer Cyclical' granger random_forest -S 2909
+python3 forecast.py 'SPX Consumer Cyclical' IAMB random_forest -S 29665
+python3 forecast.py 'SPX Consumer Cyclical' MMMB random_forest -S 226
 # commands for SPX Consumer Non cyclical
-python3 forecast.py 'SPX Consumer Non cyclical' all random_forest -S 40662
-python3 forecast.py 'SPX Consumer Non cyclical' sfi random_forest -S 31917
-python3 forecast.py 'SPX Consumer Non cyclical' mdi random_forest -S 38261
-python3 forecast.py 'SPX Consumer Non cyclical' mda random_forest -S 34286
-python3 forecast.py 'SPX Consumer Non cyclical' huang random_forest -S 5113
-python3 forecast.py 'SPX Consumer Non cyclical' granger random_forest -S 42032
-python3 forecast.py 'SPX Consumer Non cyclical' IAMB random_forest -S 21262
-python3 forecast.py 'SPX Consumer Non cyclical' MMMB random_forest -S 7866
+python3 forecast.py 'SPX Consumer Non cyclical' all random_forest -S 42178
+python3 forecast.py 'SPX Consumer Non cyclical' sfi random_forest -S 14352
+python3 forecast.py 'SPX Consumer Non cyclical' mdi random_forest -S 31039
+python3 forecast.py 'SPX Consumer Non cyclical' mda random_forest -S 31540
+python3 forecast.py 'SPX Consumer Non cyclical' huang random_forest -S 12710
+python3 forecast.py 'SPX Consumer Non cyclical' granger random_forest -S 10574
+python3 forecast.py 'SPX Consumer Non cyclical' IAMB random_forest -S 43460
+python3 forecast.py 'SPX Consumer Non cyclical' MMMB random_forest -S 27334
 # commands for SPX Energy
-python3 forecast.py 'SPX Energy' all random_forest -S 48211
-python3 forecast.py 'SPX Energy' sfi random_forest -S 6553
-python3 forecast.py 'SPX Energy' mdi random_forest -S 25509
-python3 forecast.py 'SPX Energy' mda random_forest -S 4868
-python3 forecast.py 'SPX Energy' huang random_forest -S 23045
-python3 forecast.py 'SPX Energy' granger random_forest -S 49324
-python3 forecast.py 'SPX Energy' IAMB random_forest -S 29808
-python3 forecast.py 'SPX Energy' MMMB random_forest -S 20343
+python3 forecast.py 'SPX Energy' all random_forest -S 37121
+python3 forecast.py 'SPX Energy' sfi random_forest -S 44065
+python3 forecast.py 'SPX Energy' mdi random_forest -S 39206
+python3 forecast.py 'SPX Energy' mda random_forest -S 20356
+python3 forecast.py 'SPX Energy' huang random_forest -S 7431
+python3 forecast.py 'SPX Energy' granger random_forest -S 40520
+python3 forecast.py 'SPX Energy' IAMB random_forest -S 20179
+python3 forecast.py 'SPX Energy' MMMB random_forest -S 47114
 # commands for SPX Financial
-python3 forecast.py 'SPX Financial' all random_forest -S 13654
-python3 forecast.py 'SPX Financial' sfi random_forest -S 44630
-python3 forecast.py 'SPX Financial' mdi random_forest -S 1751
-python3 forecast.py 'SPX Financial' mda random_forest -S 21427
-python3 forecast.py 'SPX Financial' huang random_forest -S 23397
-python3 forecast.py 'SPX Financial' granger random_forest -S 2050
-python3 forecast.py 'SPX Financial' IAMB random_forest -S 24127
-python3 forecast.py 'SPX Financial' MMMB random_forest -S 41118
+python3 forecast.py 'SPX Financial' all random_forest -S 43821
+python3 forecast.py 'SPX Financial' sfi random_forest -S 12008
+python3 forecast.py 'SPX Financial' mdi random_forest -S 7722
+python3 forecast.py 'SPX Financial' mda random_forest -S 29540
+python3 forecast.py 'SPX Financial' huang random_forest -S 1685
+python3 forecast.py 'SPX Financial' granger random_forest -S 30007
+python3 forecast.py 'SPX Financial' IAMB random_forest -S 9696
+python3 forecast.py 'SPX Financial' MMMB random_forest -S 26397
 # commands for SPX Industrial
-python3 forecast.py 'SPX Industrial' all random_forest -S 43313
-python3 forecast.py 'SPX Industrial' sfi random_forest -S 34279
-python3 forecast.py 'SPX Industrial' mdi random_forest -S 13107
-python3 forecast.py 'SPX Industrial' mda random_forest -S 15687
-python3 forecast.py 'SPX Industrial' huang random_forest -S 42308
-python3 forecast.py 'SPX Industrial' granger random_forest -S 40907
-python3 forecast.py 'SPX Industrial' IAMB random_forest -S 35840
-python3 forecast.py 'SPX Industrial' MMMB random_forest -S 28383
+python3 forecast.py 'SPX Industrial' all random_forest -S 7564
+python3 forecast.py 'SPX Industrial' sfi random_forest -S 26451
+python3 forecast.py 'SPX Industrial' mdi random_forest -S 45512
+python3 forecast.py 'SPX Industrial' mda random_forest -S 39380
+python3 forecast.py 'SPX Industrial' huang random_forest -S 21747
+python3 forecast.py 'SPX Industrial' granger random_forest -S 28968
+python3 forecast.py 'SPX Industrial' IAMB random_forest -S 2071
+python3 forecast.py 'SPX Industrial' MMMB random_forest -S 28362
 # commands for SPX Technology
-python3 forecast.py 'SPX Technology' all random_forest -S 46027
-python3 forecast.py 'SPX Technology' sfi random_forest -S 15856
-python3 forecast.py 'SPX Technology' mdi random_forest -S 14581
-python3 forecast.py 'SPX Technology' mda random_forest -S 38793
-python3 forecast.py 'SPX Technology' huang random_forest -S 14754
-python3 forecast.py 'SPX Technology' granger random_forest -S 33024
-python3 forecast.py 'SPX Technology' IAMB random_forest -S 6314
-python3 forecast.py 'SPX Technology' MMMB random_forest -S 36001
+python3 forecast.py 'SPX Technology' all random_forest -S 19227
+python3 forecast.py 'SPX Technology' sfi random_forest -S 23033
+python3 forecast.py 'SPX Technology' mdi random_forest -S 11620
+python3 forecast.py 'SPX Technology' mda random_forest -S 3587
+python3 forecast.py 'SPX Technology' huang random_forest -S 21944
+python3 forecast.py 'SPX Technology' granger random_forest -S 37826
+python3 forecast.py 'SPX Technology' IAMB random_forest -S 16294
+python3 forecast.py 'SPX Technology' MMMB random_forest -S 24729
 # commands for SPX Utilities
-python3 forecast.py 'SPX Utilities' all random_forest -S 11516
-python3 forecast.py 'SPX Utilities' sfi random_forest -S 37193
-python3 forecast.py 'SPX Utilities' mdi random_forest -S 47242
-python3 forecast.py 'SPX Utilities' mda random_forest -S 35748
-python3 forecast.py 'SPX Utilities' huang random_forest -S 8550
-python3 forecast.py 'SPX Utilities' granger random_forest -S 24680
-python3 forecast.py 'SPX Utilities' IAMB random_forest -S 49110
-python3 forecast.py 'SPX Utilities' MMMB random_forest -S 30009
+python3 forecast.py 'SPX Utilities' all random_forest -S 34891
+python3 forecast.py 'SPX Utilities' sfi random_forest -S 5053
+python3 forecast.py 'SPX Utilities' mdi random_forest -S 26438
+python3 forecast.py 'SPX Utilities' mda random_forest -S 24393
+python3 forecast.py 'SPX Utilities' huang random_forest -S 43397
+python3 forecast.py 'SPX Utilities' granger random_forest -S 39699
+python3 forecast.py 'SPX Utilities' IAMB random_forest -S 35944
+python3 forecast.py 'SPX Utilities' MMMB random_forest -S 14259
