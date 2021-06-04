0497e57f38182e72928af8f53cceb9d6e366cacc
felipessalvatore <felipessalvatore@github.com>
2021-06-04 13:27:40 -0300

    ädd nn3 results

--- src/nn3_forecast_script.sh
+++ src/nn3_forecast_script.sh
@@ -1,108 +1,108 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all nn3 -S 44047
-python3 forecast.py 'SPX Index' sfi nn3 -S 12272
-python3 forecast.py 'SPX Index' mdi nn3 -S 36293
-python3 forecast.py 'SPX Index' mda nn3 -S 35869
-python3 forecast.py 'SPX Index' huang nn3 -S 36175
-python3 forecast.py 'SPX Index' granger nn3 -S 1763
-python3 forecast.py 'SPX Index' IAMB nn3 -S 7522
-python3 forecast.py 'SPX Index' MMMB nn3 -S 17282
+python3 forecast.py 'SPX Index' all nn3 -S 28634
+python3 forecast.py 'SPX Index' sfi nn3 -S 29096
+python3 forecast.py 'SPX Index' mdi nn3 -S 32943
+python3 forecast.py 'SPX Index' mda nn3 -S 11985
+python3 forecast.py 'SPX Index' huang nn3 -S 49422
+python3 forecast.py 'SPX Index' granger nn3 -S 16631
+python3 forecast.py 'SPX Index' IAMB nn3 -S 32979
+python3 forecast.py 'SPX Index' MMMB nn3 -S 11408
 # commands for CCMP Index
-python3 forecast.py 'CCMP Index' all nn3 -S 14576
-python3 forecast.py 'CCMP Index' sfi nn3 -S 26908
-python3 forecast.py 'CCMP Index' mdi nn3 -S 19575
-python3 forecast.py 'CCMP Index' mda nn3 -S 45739
-python3 forecast.py 'CCMP Index' huang nn3 -S 24030
-python3 forecast.py 'CCMP Index' granger nn3 -S 34797
-python3 forecast.py 'CCMP Index' IAMB nn3 -S 9019
-python3 forecast.py 'CCMP Index' MMMB nn3 -S 26409
+python3 forecast.py 'CCMP Index' all nn3 -S 22887
+python3 forecast.py 'CCMP Index' sfi nn3 -S 1697
+python3 forecast.py 'CCMP Index' mdi nn3 -S 38273
+python3 forecast.py 'CCMP Index' mda nn3 -S 43469
+python3 forecast.py 'CCMP Index' huang nn3 -S 19520
+python3 forecast.py 'CCMP Index' granger nn3 -S 3828
+python3 forecast.py 'CCMP Index' IAMB nn3 -S 26396
+python3 forecast.py 'CCMP Index' MMMB nn3 -S 48923
 # commands for RTY Index
-python3 forecast.py 'RTY Index' all nn3 -S 40973
-python3 forecast.py 'RTY Index' sfi nn3 -S 13760
-python3 forecast.py 'RTY Index' mdi nn3 -S 5150
-python3 forecast.py 'RTY Index' mda nn3 -S 41767
-python3 forecast.py 'RTY Index' huang nn3 -S 24535
-python3 forecast.py 'RTY Index' granger nn3 -S 4396
-python3 forecast.py 'RTY Index' IAMB nn3 -S 13566
-python3 forecast.py 'RTY Index' MMMB nn3 -S 38759
+python3 forecast.py 'RTY Index' all nn3 -S 37607
+python3 forecast.py 'RTY Index' sfi nn3 -S 34586
+python3 forecast.py 'RTY Index' mdi nn3 -S 14838
+python3 forecast.py 'RTY Index' mda nn3 -S 47617
+python3 forecast.py 'RTY Index' huang nn3 -S 4212
+python3 forecast.py 'RTY Index' granger nn3 -S 1435
+python3 forecast.py 'RTY Index' IAMB nn3 -S 39439
+python3 forecast.py 'RTY Index' MMMB nn3 -S 19213
 # commands for SPX Basic Materials
-python3 forecast.py 'SPX Basic Materials' all nn3 -S 44138
-python3 forecast.py 'SPX Basic Materials' sfi nn3 -S 5357
-python3 forecast.py 'SPX Basic Materials' mdi nn3 -S 36465
-python3 forecast.py 'SPX Basic Materials' mda nn3 -S 38213
-python3 forecast.py 'SPX Basic Materials' huang nn3 -S 40373
-python3 forecast.py 'SPX Basic Materials' granger nn3 -S 44978
-python3 forecast.py 'SPX Basic Materials' IAMB nn3 -S 41599
-python3 forecast.py 'SPX Basic Materials' MMMB nn3 -S 40551
+python3 forecast.py 'SPX Basic Materials' all nn3 -S 31306
+python3 forecast.py 'SPX Basic Materials' sfi nn3 -S 6232
+python3 forecast.py 'SPX Basic Materials' mdi nn3 -S 26292
+python3 forecast.py 'SPX Basic Materials' mda nn3 -S 31439
+python3 forecast.py 'SPX Basic Materials' huang nn3 -S 18833
+python3 forecast.py 'SPX Basic Materials' granger nn3 -S 9852
+python3 forecast.py 'SPX Basic Materials' IAMB nn3 -S 30582
+python3 forecast.py 'SPX Basic Materials' MMMB nn3 -S 39308
 # commands for SPX Communications
-python3 forecast.py 'SPX Communications' all nn3 -S 22500
-python3 forecast.py 'SPX Communications' sfi nn3 -S 33501
-python3 forecast.py 'SPX Communications' mdi nn3 -S 24039
-python3 forecast.py 'SPX Communications' mda nn3 -S 237
-python3 forecast.py 'SPX Communications' huang nn3 -S 9475
-python3 forecast.py 'SPX Communications' granger nn3 -S 11942
-python3 forecast.py 'SPX Communications' IAMB nn3 -S 14412
-python3 forecast.py 'SPX Communications' MMMB nn3 -S 45173
+python3 forecast.py 'SPX Communications' all nn3 -S 7994
+python3 forecast.py 'SPX Communications' sfi nn3 -S 48490
+python3 forecast.py 'SPX Communications' mdi nn3 -S 31583
+python3 forecast.py 'SPX Communications' mda nn3 -S 44907
+python3 forecast.py 'SPX Communications' huang nn3 -S 29668
+python3 forecast.py 'SPX Communications' granger nn3 -S 707
+python3 forecast.py 'SPX Communications' IAMB nn3 -S 39817
+python3 forecast.py 'SPX Communications' MMMB nn3 -S 47669
 # commands for SPX Consumer Cyclical
-python3 forecast.py 'SPX Consumer Cyclical' all nn3 -S 8985
-python3 forecast.py 'SPX Consumer Cyclical' sfi nn3 -S 30120
-python3 forecast.py 'SPX Consumer Cyclical' mdi nn3 -S 20664
-python3 forecast.py 'SPX Consumer Cyclical' mda nn3 -S 39282
-python3 forecast.py 'SPX Consumer Cyclical' huang nn3 -S 3782
-python3 forecast.py 'SPX Consumer Cyclical' granger nn3 -S 5604
-python3 forecast.py 'SPX Consumer Cyclical' IAMB nn3 -S 41059
-python3 forecast.py 'SPX Consumer Cyclical' MMMB nn3 -S 42486
+python3 forecast.py 'SPX Consumer Cyclical' all nn3 -S 30279
+python3 forecast.py 'SPX Consumer Cyclical' sfi nn3 -S 17330
+python3 forecast.py 'SPX Consumer Cyclical' mdi nn3 -S 37097
+python3 forecast.py 'SPX Consumer Cyclical' mda nn3 -S 49893
+python3 forecast.py 'SPX Consumer Cyclical' huang nn3 -S 23087
+python3 forecast.py 'SPX Consumer Cyclical' granger nn3 -S 17303
+python3 forecast.py 'SPX Consumer Cyclical' IAMB nn3 -S 17546
+python3 forecast.py 'SPX Consumer Cyclical' MMMB nn3 -S 12099
 # commands for SPX Consumer Non cyclical
-python3 forecast.py 'SPX Consumer Non cyclical' all nn3 -S 29646
-python3 forecast.py 'SPX Consumer Non cyclical' sfi nn3 -S 6677
-python3 forecast.py 'SPX Consumer Non cyclical' mdi nn3 -S 35698
-python3 forecast.py 'SPX Consumer Non cyclical' mda nn3 -S 38774
-python3 forecast.py 'SPX Consumer Non cyclical' huang nn3 -S 5511
-python3 forecast.py 'SPX Consumer Non cyclical' granger nn3 -S 30434
-python3 forecast.py 'SPX Consumer Non cyclical' IAMB nn3 -S 17650
-python3 forecast.py 'SPX Consumer Non cyclical' MMMB nn3 -S 10774
+python3 forecast.py 'SPX Consumer Non cyclical' all nn3 -S 8031
+python3 forecast.py 'SPX Consumer Non cyclical' sfi nn3 -S 23026
+python3 forecast.py 'SPX Consumer Non cyclical' mdi nn3 -S 19997
+python3 forecast.py 'SPX Consumer Non cyclical' mda nn3 -S 3770
+python3 forecast.py 'SPX Consumer Non cyclical' huang nn3 -S 21225
+python3 forecast.py 'SPX Consumer Non cyclical' granger nn3 -S 27613
+python3 forecast.py 'SPX Consumer Non cyclical' IAMB nn3 -S 31617
+python3 forecast.py 'SPX Consumer Non cyclical' MMMB nn3 -S 16980
 # commands for SPX Energy
-python3 forecast.py 'SPX Energy' all nn3 -S 23081
-python3 forecast.py 'SPX Energy' sfi nn3 -S 18379
-python3 forecast.py 'SPX Energy' mdi nn3 -S 4189
-python3 forecast.py 'SPX Energy' mda nn3 -S 31643
-python3 forecast.py 'SPX Energy' huang nn3 -S 24133
-python3 forecast.py 'SPX Energy' granger nn3 -S 43122
-python3 forecast.py 'SPX Energy' IAMB nn3 -S 22532
-python3 forecast.py 'SPX Energy' MMMB nn3 -S 45370
+python3 forecast.py 'SPX Energy' all nn3 -S 6316
+python3 forecast.py 'SPX Energy' sfi nn3 -S 4245
+python3 forecast.py 'SPX Energy' mdi nn3 -S 7464
+python3 forecast.py 'SPX Energy' mda nn3 -S 27669
+python3 forecast.py 'SPX Energy' huang nn3 -S 30041
+python3 forecast.py 'SPX Energy' granger nn3 -S 13953
+python3 forecast.py 'SPX Energy' IAMB nn3 -S 46532
+python3 forecast.py 'SPX Energy' MMMB nn3 -S 7651
 # commands for SPX Financial
-python3 forecast.py 'SPX Financial' all nn3 -S 11329
-python3 forecast.py 'SPX Financial' sfi nn3 -S 36990
-python3 forecast.py 'SPX Financial' mdi nn3 -S 34821
-python3 forecast.py 'SPX Financial' mda nn3 -S 13922
-python3 forecast.py 'SPX Financial' huang nn3 -S 1117
-python3 forecast.py 'SPX Financial' granger nn3 -S 12626
-python3 forecast.py 'SPX Financial' IAMB nn3 -S 48210
-python3 forecast.py 'SPX Financial' MMMB nn3 -S 47805
+python3 forecast.py 'SPX Financial' all nn3 -S 13682
+python3 forecast.py 'SPX Financial' sfi nn3 -S 17873
+python3 forecast.py 'SPX Financial' mdi nn3 -S 36935
+python3 forecast.py 'SPX Financial' mda nn3 -S 4524
+python3 forecast.py 'SPX Financial' huang nn3 -S 15280
+python3 forecast.py 'SPX Financial' granger nn3 -S 18743
+python3 forecast.py 'SPX Financial' IAMB nn3 -S 37786
+python3 forecast.py 'SPX Financial' MMMB nn3 -S 38652
 # commands for SPX Industrial
-python3 forecast.py 'SPX Industrial' all nn3 -S 37989
-python3 forecast.py 'SPX Industrial' sfi nn3 -S 1058
-python3 forecast.py 'SPX Industrial' mdi nn3 -S 34372
-python3 forecast.py 'SPX Industrial' mda nn3 -S 16058
-python3 forecast.py 'SPX Industrial' huang nn3 -S 8437
-python3 forecast.py 'SPX Industrial' granger nn3 -S 4947
-python3 forecast.py 'SPX Industrial' IAMB nn3 -S 25464
-python3 forecast.py 'SPX Industrial' MMMB nn3 -S 41169
+python3 forecast.py 'SPX Industrial' all nn3 -S 41628
+python3 forecast.py 'SPX Industrial' sfi nn3 -S 37763
+python3 forecast.py 'SPX Industrial' mdi nn3 -S 45368
+python3 forecast.py 'SPX Industrial' mda nn3 -S 13967
+python3 forecast.py 'SPX Industrial' huang nn3 -S 313
+python3 forecast.py 'SPX Industrial' granger nn3 -S 39561
+python3 forecast.py 'SPX Industrial' IAMB nn3 -S 32250
+python3 forecast.py 'SPX Industrial' MMMB nn3 -S 17618
 # commands for SPX Technology
-python3 forecast.py 'SPX Technology' all nn3 -S 26465
-python3 forecast.py 'SPX Technology' sfi nn3 -S 5888
-python3 forecast.py 'SPX Technology' mdi nn3 -S 31272
-python3 forecast.py 'SPX Technology' mda nn3 -S 38480
-python3 forecast.py 'SPX Technology' huang nn3 -S 20604
-python3 forecast.py 'SPX Technology' granger nn3 -S 27974
-python3 forecast.py 'SPX Technology' IAMB nn3 -S 12667
-python3 forecast.py 'SPX Technology' MMMB nn3 -S 3037
+python3 forecast.py 'SPX Technology' all nn3 -S 14939
+python3 forecast.py 'SPX Technology' sfi nn3 -S 7373
+python3 forecast.py 'SPX Technology' mdi nn3 -S 33426
+python3 forecast.py 'SPX Technology' mda nn3 -S 31850
+python3 forecast.py 'SPX Technology' huang nn3 -S 23433
+python3 forecast.py 'SPX Technology' granger nn3 -S 18048
+python3 forecast.py 'SPX Technology' IAMB nn3 -S 18566
+python3 forecast.py 'SPX Technology' MMMB nn3 -S 30015
 # commands for SPX Utilities
-python3 forecast.py 'SPX Utilities' all nn3 -S 22175
-python3 forecast.py 'SPX Utilities' sfi nn3 -S 20969
-python3 forecast.py 'SPX Utilities' mdi nn3 -S 36743
-python3 forecast.py 'SPX Utilities' mda nn3 -S 28584
-python3 forecast.py 'SPX Utilities' huang nn3 -S 34588
-python3 forecast.py 'SPX Utilities' granger nn3 -S 13034
-python3 forecast.py 'SPX Utilities' IAMB nn3 -S 1200
-python3 forecast.py 'SPX Utilities' MMMB nn3 -S 352
+python3 forecast.py 'SPX Utilities' all nn3 -S 12664
+python3 forecast.py 'SPX Utilities' sfi nn3 -S 27948
+python3 forecast.py 'SPX Utilities' mdi nn3 -S 10249
+python3 forecast.py 'SPX Utilities' mda nn3 -S 34582
+python3 forecast.py 'SPX Utilities' huang nn3 -S 21365
+python3 forecast.py 'SPX Utilities' granger nn3 -S 11398
+python3 forecast.py 'SPX Utilities' IAMB nn3 -S 10065
+python3 forecast.py 'SPX Utilities' MMMB nn3 -S 16324
