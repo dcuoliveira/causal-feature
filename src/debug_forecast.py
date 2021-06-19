4b24b2fb8aa7257225766a216b0b497db2fb24af
dcuoliveira <dan.cunhaa@gmail.com>
2021-06-19 10:09:52 -0300

    refactor dir

--- src/debug_forecast.py
+++ src/debug_forecast.py
@@ -10,19 +10,20 @@ from prediction.functions import forecast
 if __name__ == '__main__':
     
     init = time()
-    pred_results = forecast(ticker_name="SPX Index",
-                            fs_method="sfi",
-                            Wrapper=LogisticRegWrapper,
-                            n_iter=10,
-                            n_splits=5,
-                            n_jobs=-1,
-                            seed=2294,
-                            verbose=1,
-                            is_discrete=True)
-    pred_results.to_csv(os.path.join("results",
-                                     "forecast",
-                                     "sfi",
-                                     "indices",
-                                     "SPX Index.csv"))
+    pred_results, importance_results = forecast(ticker_name="SPX Index",
+                                                fs_method="huang",
+                                                Wrapper=RandomForestWrapper,
+                                                n_iter=10,
+                                                n_splits=5,
+                                                n_jobs=-1,
+                                                seed=2294,
+                                                verbose=1)
+    
+     importance_results.to_csv(os.path.join("results",
+                                            "feature_importance",
+                                            "sfi",
+                                            "indices",
+                                            "random_forest",
+                                            "SPX Index.csv"))
     tempo = (time() - init) / 60
     print("total run time = ", np.round(tempo, 2), "min")  
