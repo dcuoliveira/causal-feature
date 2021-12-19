e989b81299531aa40a1e482f98c95b98bd0243a6
dcuoliveira <dan.cunhaa@gmail.com>
2021-12-18 21:34:21 -0300

    add nncomb wrapper

--- src/debug_forecast_comb.py
+++ src/debug_forecast_comb.py
@@ -1,5 +1,6 @@
 import os
 from prediction.functions import forecast_comb
+from prediction.models import NNCombWrapper
 
 comb_models = ['nncomb'] # ['average', 'median', 'bates_granger', 'nncomb']
 models = ['logit', 'ridge', 'lasso', 'enet', 'random_forest', 'lgb', 'nn3']
@@ -16,15 +17,36 @@ if __name__ == '__main__':
     for f in fs_methods:
         for c in comb_models:
             for t in tickers:
-                pred_results = forecast_comb(ticker_name=t,
-                                             fs_method=f,
-                                             model=models,
-                                             start_date=start_date,
-                                             end_date=end_date,
-                                             metric_name=metric,
-                                             comb_model=c,
-                                             target_name='target_return',
-                                             benchmark_name='return_direction')
+                if c == 'nncomb':
+                    pred_results = forecast_comb(ticker_name=t,
+                                                 fs_method=f,
+                                                 model=models,
+                                                 start_date=start_date,
+                                                 end_date=end_date,
+                                                 metric_name=metric,
+                                                 comb_model=c,
+                                                 target_name='target_return',
+                                                 benchmark_name='return_direction',
+                                                 Wrapper=NNCombWrapper,
+                                                 n_iter=10,
+                                                 n_jobs=-1,
+                                                 n_splits=5,
+                                                 seed=2294)
+                else:
+                    pred_results = forecast_comb(ticker_name=t,
+                                                 fs_method=f,
+                                                 model=models,
+                                                 start_date=start_date,
+                                                 end_date=end_date,
+                                                 metric_name=metric,
+                                                 comb_model=c,
+                                                 target_name='target_return',
+                                                 benchmark_name='return_direction',
+                                                 Wrapper=None,
+                                                 n_iter=None,
+                                                 n_jobs=None,
+                                                 n_splits=None,
+                                                 seed=None)
                 pred_results.to_csv(os.path.join("results",
                                                  "forecast",
                                                  f,
