615945d80a7f707e8fb3ba481ebfaf1d7105fb94
dcuoliveira <dan.cunhaa@gmail.com>
2022-11-14 11:08:59 -0300

    first commit ent and rf

---
+++ src/enet_forecast_script.sh
@@ -0,0 +1,4 @@
+# commands for SPX Index
+python3 forecast.py 'SPX Index' all enet -S 3452 -i 1
+python3 forecast.py 'SPX Index' huang enet -S 21230 -i 1
+python3 forecast.py 'SPX Index' granger enet -S 18212 -i 1
