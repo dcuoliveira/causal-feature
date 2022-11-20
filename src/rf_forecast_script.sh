7a63277d351ffeb0d335037d3f6c00471391b8de
dcuoliveira <dan.cunhaa@gmail.com>
2022-11-20 20:44:40 -0300

    fix rf runner

--- src/rf_forecast_script.sh
+++
@@ -1,4 +0,0 @@
-# commands for SPX Index
-python3 forecast.py 'SPX Index' all rf -S 3452 -i 1
-python3 forecast.py 'SPX Index' huang rf -S 21230 -i 1
-python3 forecast.py 'SPX Index' granger rf -S 18212 -i 1
