f5ce09c0e3a0529ea219a443686a131f027736ca
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-16 06:14:41 -0300

    add lasso

---
+++ src/lasso_forecast_script copy.sh
@@ -0,0 +1,4 @@
+# commands for SPX Index
+python3 forecast.py 'SPX Index' all lasso 252 5 -S 3452 -i 1
+python3 forecast.py 'SPX Index' huang lasso 252 5 -S 21230 -i 1
+python3 forecast.py 'SPX Index' granger lasso 252 5 -S 18212 -i 1
