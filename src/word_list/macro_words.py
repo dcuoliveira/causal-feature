64e2601fc5a1a07967d302a8917f694961b1b3ee
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-21 00:06:21 -0300

    add

---
+++ src/word_list/macro_words.py
@@ -0,0 +1,23 @@
+crisis_recession = ["Economic crisis", "Crisis", "Recession", "Financial crisis", 'Krach']
+
+employment = ["Unemployment", "Unemployment benefits", "Welfare & Unemployment"]
+
+credit = ['Student loan', 'Credit & Lending', 'Loan', 'Interest', 'Mortgage', 'Auto Financing']
+
+consumption = ['Food & Drink', 'GPS & Navigation', 'Performing Arts','Luggage topic', 'Vehicle Brands', 'Birthday', 'Travel', 'Energy & Utilities', 'Vehicle Shopping', 'Tobacco Products', 'Health', 'Pharmacy', 'Carpooling & Ridesharing', 'Sports', 'Animal Products & Services', 'Fitness', 'Weddings', 'Car Rental & Taxi Services', 'Autos & Vehicles', 'Tourist Destinations', 'Home & Garden', 'Events & Listings', 'Grocery & Food Retailers', 'Vehicle Licensing & Registration', 'Timeshares & Vacation Properties', 'Home Appliances', 'Mass Merchants & Department Stores', 'Car Electronics', 'Fashion & Style', 'Trucks & SUVs', 'Home Furnishings', 'Footwear', 'Cruises & Charters', 'Hotels & Accommodations', 'Luggage & Travel Accessories', 'Fast Food', 'Book Retailers', 'Veterinarians', 'Spas & Beauty Services', 'Acting & Theater', 'Travel Agencies & Services']
+
+jobs = ['Waiter', 'Job Listings', 'Resumes & Portfolios', 'Jobs topic', 'Temporary jobs', 'Private employment agency', 'Recruitement', 'Developer Jobs', 'Job search']
+
+bankruptcy = ['Bankruptcy topic', 'Judicial Liquidation', 'Bankruptcy']
+
+housing = ['Affordable housing', 'House price index', 'Apartments & Residential Rentals', 'Home Insurance', 'Home Improvement']
+
+news_politics = ['Economy News', 'Business News', 'World News', 'Politics', 'Newspapers']
+
+construction = ['Flooring', 'Construction Consulting & Contracting', 'Swimming Pools & Spas', 'Civil Engineering', 'Construction & Maintenance']
+
+personal_finance = ['Investment', 'Investing', 'Financial Planning']
+
+business_services = ['Data Management', 'Enterprise Technology', 'Accounting & Auditing', 'CAD & CAM', 'Development Tools', 'Customer Relationship Management (CRM)', 'Printing & Publishing', 'Corporate Events', 'Computer Security', 'Outsourcing', 'Distribution & Logistics', 'Computer Servers', 'Consulting', 'Web Hosting & Domain Registration', 'Enterprise Resource Planning (ERP)', 'Business Operations', 'Commercial Vehicles']
+
+insdustrial_activity = ['Agriculture & Forestry', 'Agrochemicals', 'Aviation', 'Business & Industrial', 'Chemicals Industry', 'Textiles & Nonwovens', 'Coatings & Adhesives', 'Food Production', 'Dyes & Pigments', 'Freight & Trucking', 'Transportation & Logistics', 'Mail & Package Delivery', 'Manufacturing']
