fe6a30ebb3ca2ee524d1b8ed2d1c515bffbd4be7
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-11 18:52:36 -0300

    fix parameters

--- src/logit_forecast_dyn_fs_script.sh
+++ src/logit_forecast_dyn_fs_script.sh
@@ -1,3 +1,3 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' huang logit 252 5 -S 21230 -i 1 -d true
-python3 forecast.py 'SPX Index' granger logit 252 5 -S 18212 -i 1 -d true
+python3 forecast.py 'SPX Index' huang logit 52 1 -S 21230 -i 1 -d true
+python3 forecast.py 'SPX Index' granger logit 52 1 -S 18212 -i 1 -d true
