e0c0b6a503462367f804b4e97bf1e03977f67148
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-11 19:01:17 -0300

    fix parameters

--- src/lasso_forecast_dyn_fs_script.sh
+++ src/lasso_forecast_dyn_fs_script.sh
@@ -1,3 +1,3 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' huang lasso 252 5 -S 21230 -i 1 -d true
-python3 forecast.py 'SPX Index' granger lasso 252 5 -S 18212 -i 1 -d true
+python3 forecast.py 'SPX Index' huang lasso 104 1 -S 21230 -i 50 -d true
+python3 forecast.py 'SPX Index' granger lasso 104 1 -S 18212 -i 50 -d true
--- src/logit_forecast_dyn_fs_script.sh
+++ src/logit_forecast_dyn_fs_script.sh
@@ -1,3 +1,3 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' huang logit 52 1 -S 21230 -i 1 -d true
-python3 forecast.py 'SPX Index' granger logit 52 1 -S 18212 -i 1 -d true
+python3 forecast.py 'SPX Index' huang logit 104 1 -S 21230 -i 1 -d true
+python3 forecast.py 'SPX Index' granger logit 104 1 -S 18212 -i 1 -d true
