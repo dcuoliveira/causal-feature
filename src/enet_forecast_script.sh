4f5e9c09b2343243e19d065e854b9a43574be131
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-11 19:03:23 -0300

    fix static parameters

--- src/enet_forecast_script.sh
+++ src/enet_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all enet 252 5 -S 3452 -i 1
-python3 forecast.py 'SPX Index' huang enet 252 5 -S 21230 -i 1
-python3 forecast.py 'SPX Index' granger enet 252 5 -S 18212 -i 1
+python3 forecast.py 'SPX Index' all enet 104 1 -S 3452 -i 50 -d false
+python3 forecast.py 'SPX Index' huang enet 104 1 -S 21230 -i 50 -d false
+python3 forecast.py 'SPX Index' granger enet 104 1 -S 18212 -i 50 -d false
--- src/lasso_forecast_script.sh
+++ src/lasso_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all lasso 252 5 -S 3452 -i 1
-python3 forecast.py 'SPX Index' huang lasso 252 5 -S 21230 -i 1
-python3 forecast.py 'SPX Index' granger lasso 252 5 -S 18212 -i 1
+python3 forecast.py 'SPX Index' all lasso 104 1 -S 3452 -i 50 -d false
+python3 forecast.py 'SPX Index' huang lasso 104 1 -S 21230 -i 50 -d false
+python3 forecast.py 'SPX Index' granger lasso 104 1 -S 18212 -i 50 -d false
