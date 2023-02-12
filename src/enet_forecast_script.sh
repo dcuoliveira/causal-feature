8c5013adf4c325bdaae8497d85f2a31f341710cc
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-12 17:52:34 -0300

    fix parameters

--- src/enet_forecast_script.sh
+++ src/enet_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all enet 104 1 -S 3452 -i 50 -d false
-python3 forecast.py 'SPX Index' huang enet 104 1 -S 21230 -i 50 -d false
-python3 forecast.py 'SPX Index' granger enet 104 1 -S 18212 -i 50 -d false
+python3 forecast.py 'SPX Index' all enet 104 1 -S 3452 -i 50
+python3 forecast.py 'SPX Index' huang enet 104 1 -S 21230 -i 50
+python3 forecast.py 'SPX Index' granger enet 104 1 -S 18212 -i 50
