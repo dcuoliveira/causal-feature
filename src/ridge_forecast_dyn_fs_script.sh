7e97e0c12cf83af7519a4877c0fa96b9e4313fe7
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-11 22:05:04 -0300

    first commmit rige run

---
+++ src/ridge_forecast_dyn_fs_script.sh
@@ -0,0 +1,3 @@
+# commands for SPX Index
+python3 forecast.py 'SPX Index' huang ridge 104 1 -S 21230 -i 50 -d true
+python3 forecast.py 'SPX Index' granger ridge 104 1 -S 18212 -i 50 -d true
---
+++ src/ridge_forecast_script.sh
@@ -0,0 +1,4 @@
+# commands for SPX Index
+python3 forecast.py 'SPX Index' all ridge 104 1 -S 3452 -i 50 -d false
+python3 forecast.py 'SPX Index' huang ridge 104 1 -S 21230 -i 50 -d false
+python3 forecast.py 'SPX Index' granger ridge 104 1 -S 18212 -i 50 -d false
