ecfee7276f8c0b41fb3ffcf93d0ec885fcb665e7
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-12 17:44:35 -0300

    fix dynfs parameter

--- src/logit_forecast_script.sh
+++ src/logit_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all logit 104 1 -S 3452 -i 1 -d false
-python3 forecast.py 'SPX Index' huang logit 104 1 -S 21230 -i 1 -d false
-python3 forecast.py 'SPX Index' granger logit 104 1 -S 18212 -i 1 -d false
+python3 forecast.py 'SPX Index' all logit 104 1 -S 3452 -i 1
+python3 forecast.py 'SPX Index' huang logit 104 1 -S 21230 -i 1
+python3 forecast.py 'SPX Index' granger logit 104 1 -S 18212 -i 1
--- src/random_forest_forecast_script.sh
+++ src/random_forest_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all random_forest 104 1 -S 44467 -i 50 -d false
-python3 forecast.py 'SPX Index' huang random_forest 104 1 -S 45205 -i 50 -d false
-python3 forecast.py 'SPX Index' granger random_forest 104 1 -S 34783 -i 50 -d false
+python3 forecast.py 'SPX Index' all random_forest 104 1 -S 44467 -i 50
+python3 forecast.py 'SPX Index' huang random_forest 104 1 -S 45205 -i 50
+python3 forecast.py 'SPX Index' granger random_forest 104 1 -S 34783 -i 50
