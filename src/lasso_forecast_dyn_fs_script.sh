1d9c05a928a924fab538b1f0c56198f44fe5662a
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-12 08:43:12 -0300

    add all to dyn fs

--- src/lasso_forecast_dyn_fs_script.sh
+++ src/lasso_forecast_dyn_fs_script.sh
@@ -1,3 +1,4 @@
 # commands for SPX Index
+python3 forecast.py 'SPX Index' all lasso 104 1 -S 21230 -i 50 -d true
 python3 forecast.py 'SPX Index' huang lasso 104 1 -S 21230 -i 50 -d true
 python3 forecast.py 'SPX Index' granger lasso 104 1 -S 18212 -i 50 -d true
--- src/logit_forecast_dyn_fs_script.sh
+++ src/logit_forecast_dyn_fs_script.sh
@@ -1,3 +1,4 @@
 # commands for SPX Index
+python3 forecast.py 'SPX Index' all logit 104 1 -S 21230 -i 1 -d true
 python3 forecast.py 'SPX Index' huang logit 104 1 -S 21230 -i 1 -d true
 python3 forecast.py 'SPX Index' granger logit 104 1 -S 18212 -i 1 -d true
