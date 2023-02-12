e85ac3e26d3ba51df15512b9c264dcd378919c80
dcuoliveira <dan.cunhaa@gmail.com>
2023-02-12 08:28:16 -0300

    add random forest

---
+++ src/random_forest_forecast_dyn_fs_script.sh
@@ -0,0 +1,4 @@
+# commands for SPX Index
+python3 forecast.py 'SPX Index' all random_forest 104 1 -S 44467 -i 50 -d true
+python3 forecast.py 'SPX Index' huang random_forest 104 1 -S 45205 -i 50 -d true
+python3 forecast.py 'SPX Index' granger random_forest 104 1 -S 34783 -i 50 -d true
--- src/random_forest_forecast_script.sh
+++ src/random_forest_forecast_script.sh
@@ -1,4 +1,4 @@
 # commands for SPX Index
-python3 forecast.py 'SPX Index' all random_forest 252 5 -S 44467
-python3 forecast.py 'SPX Index' huang random_forest 252 5 -S 45205
-python3 forecast.py 'SPX Index' granger random_forest 252 5 -S 34783
+python3 forecast.py 'SPX Index' all random_forest 104 1 -S 44467 -i 50 -d false
+python3 forecast.py 'SPX Index' huang random_forest 104 1 -S 45205 -i 50 -d false
+python3 forecast.py 'SPX Index' granger random_forest 104 1 -S 34783 -i 50 -d false
